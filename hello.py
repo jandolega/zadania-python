5542f0def8a582646f218591588fedd6f13c6307
jandolega <jasdolega@gmail.com>
2022-11-23 19:10:04 +0100

    Start

---
+++ hello.py
@@ -0,0 +1,24 @@
+print("Poczatek zadania")
+
+def roznica():
+    print (2537-1887)
+    
+def winda():
+    pojemnosc = 1000
+    waga_gruszki = 0.5/30
+    print(waga_gruszki*590<1000)
+
+def nasiona():
+    ala = 50
+    adam = 12
+    zuzia = 29
+    print (ala+adam+zuzia)
+
+
+
+
+
+nasiona()
+
+
+
